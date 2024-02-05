9e39f47760278cb324dc2862beda7236c6ad1059
muneebuforia <muneeb@uforiainfotech.com>
2024-02-05 17:43:17 +0500

    Passport API Code Completed

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      */
