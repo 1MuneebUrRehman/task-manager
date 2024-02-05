e05cd87082e66be66664edb285be068dc31d7a6d
muneebuforia <muneeb@uforiainfotech.com>
2024-02-05 11:01:52 +0500

    Users column removed from navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,15 +21,6 @@
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('users.index') }}" class="nav-link">
-                    <i class="nav-icon fas fa-users"></i>
-                    <p>
-                        {{ __('Users') }}
-                    </p>
-                </a>
-            </li>
-
-            <li class="nav-item">
                 <a href="{{ route('task.index') }}" class="nav-link">
                     <i class="nav-icon fas fa-users"></i>
                     <p>
